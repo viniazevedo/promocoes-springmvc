fc97c8b4c69dba7f97cfa8fc1d0e59b863089a45
Vinicius <vini.azevedo@ymail.com>
2019-05-04 23:51:01 -0300

    Listando as promoções

--- src/main/java/com/loja/demo/controller/PromocaoController.java
+++ src/main/java/com/loja/demo/controller/PromocaoController.java
@@ -10,8 +10,11 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,6 +56,16 @@ public class PromocaoController {
 		log.info("Promoção salva.");
 		return ResponseEntity.ok().build();
 	}
+	
+	
+	@GetMapping("/list")
+	public String listarOfertas(ModelMap model) {
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		return "promo-list";
+	}
+	
 	
 	@ModelAttribute("categorias")
 	public List<Categoria> getCategorias(){
